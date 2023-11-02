e7045a286a00949efd34da266989b07260082f58
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-11-02 13:55:18 -0700

    made pseudocode

---
+++ script.js
@@ -0,0 +1,30 @@
+// Pull the input, form and ul from the html
+
+
+// add event listener to the form to look for clicks
+
+// remove the function's default
+
+// Assign a variable to the user input
+
+// Create a new list item
+
+// Create a span inside of the new list item
+
+// Create a button to remove list items
+
+// Add the text "remove" to the button
+
+// Add event listener to watch the remove button for clicks
+
+    // target the click event
+
+    // target the button
+
+    // remove list item
+
+// add the remove button to the new list item
+
+// clear the input after submission
+
+// focus back to the input
